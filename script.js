4925908d92cc9958d7785b3b08e09055251ea857
Anthony Phan <89223273+anthphan@users.noreply.github.com>
2024-05-12 05:48:01 -0400

    made it responsive

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav__links');
+    var hamburgerMenu = document.querySelector('.hamburger__menu');
+    navLinks.classList.toggle('show');
+    hamburgerMenu.classList.toggle('active');
+}
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
